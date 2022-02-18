f070a532932bf9ac3d1bce9d92b7e303da2db3cd
Daniel Murrow <dsmurrow@gmail.com>
2022-02-18 12:04:54 -0500

    replaced all instances of 'const char*' with 'uint8_t'

--- sha3.h
+++ sha3.h
@@ -3,13 +3,13 @@
 
 #include <inttypes.h>
 
-uint8_t *sha3_224(const char*, uint32_t);
-uint8_t *sha3_256(const char*, uint32_t);
-uint8_t *sha3_384(const char*, uint32_t);
-uint8_t *sha3_512(const char*, uint32_t);
+uint8_t *sha3_224(uint8_t*, uint32_t);
+uint8_t *sha3_256(uint8_t*, uint32_t);
+uint8_t *sha3_384(uint8_t*, uint32_t);
+uint8_t *sha3_512(uint8_t*, uint32_t);
 
-uint8_t *shake_128(const char*, uint32_t, uint32_t);
-uint8_t *shake_256(const char*, uint32_t, uint32_t);
+uint8_t *shake_128(uint8_t*, uint32_t, uint32_t);
+uint8_t *shake_256(uint8_t*, uint32_t, uint32_t);
 
 #endif
 
